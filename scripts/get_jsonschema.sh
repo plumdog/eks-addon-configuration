bec48cde8411f6a98366c1802eece025474fbad8
Andrew Plummer <plummer574@gmail.com>
2023-09-30 12:16:13 +0100

    Single data file

--- scripts/get_jsonschema.sh
+++ scripts/get_jsonschema.sh
@@ -1,13 +1,24 @@
 #!/bin/bash -e
 
+export AWS_MAX_ATTEMPTS=10
+
 script_dir="$(dirname "$(readlink -f "$0")")"
 project_dir="$(dirname "$script_dir")"
 data_dir="$project_dir/data/"
+
+rm -rf "$data_dir"
 mkdir -p "$data_dir"
 
 addons="$(aws eks describe-addon-versions --region us-east-1 --query addons --output json)"
 
 aws eks describe-addon-versions --region us-east-1 --query addons --output json | jq 'map(.addonName)' > "$data_dir/addons.json"
+
+function writeAddonVersionConfiguration() {
+    addonName="$1"
+    addonVersion="$2"
+    configurationsDir="$3"
+    aws eks describe-addon-configuration --region us-east-1 --addon-name "$addonName" --addon-version "$addonVersion" --query configurationSchema --output text | jq . > "$configurationsDir/$addonVersion.json"
+}
 
 for addonBase64 in $(echo "$addons" | jq -r '.[] | @base64'); do
     addon="$(echo "$addonBase64" | base64 --decode | jq .)"
@@ -21,6 +32,21 @@ for addonBase64 in $(echo "$addons" | jq -r '.[] | @base64'); do
 
     for addonVersionBase64 in $(echo "$addon" | jq -r '.addonVersions[] | .addonVersion | @base64'); do
         addonVersion="$(echo "$addonVersionBase64" | base64 -d)"
-        aws eks describe-addon-configuration --region us-east-1 --addon-name "$addonName" --addon-version "$addonVersion" --query configurationSchema --output text | jq . > "$configurationsDir/$addonVersion.json"
+        writeAddonVersionConfiguration "$addonName" "$addonVersion" "$configurationsDir" &
     done
 done
+
+while true; do
+    jobs_remaining="$(jobs -r | wc -l)"
+    if [[ $jobs_remaining == 0 ]]; then
+        break
+    fi
+
+    echo "$jobs_remaining jobs remaining"
+    sleep 1
+    wait -n
+done
+
+wait
+
+(cd "$data_dir" && find . -name '*.json' | while read filepath; do cat "$filepath" | jq --arg filepath "$filepath" '{$filepath: .}'; done | jq -s 'add' > data.json)
