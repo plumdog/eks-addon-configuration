758e338222e4deb9229fa3adc0d6a16ca3156661
Andrew Plummer <plummer574@gmail.com>
2023-10-13 16:32:34 +0100

    Fix build (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ function App() {
                 }
             }
         }
-    });
+    }, [selectedAddonName, selectedAddonData, selectedAddonVersion]);
 
     const handleAddonSelect = (addonName: string) => {
         setSelectedAddonData(null);
@@ -94,7 +94,7 @@ function App() {
         if (selectedAddonName && selectedAddonVersion && !selectedAddonVersionConfiguration) {
             setSelectedAddonVersionConfiguration(data[`./${selectedAddonName}/configurations/${selectedAddonVersion}.json`]);
         }
-    });
+    }, [selectedAddonName, selectedAddonVersion, selectedAddonVersionConfiguration]);
 
     return (
         <div className="App">
