9868eb468783b898d75495302dedab3257e3cdab
Andrew Plummer <plummer574@gmail.com>
2023-10-20 16:14:39 +0100

    Remove 'Human' tab (#10)

--- src/AddonViewer.tsx
+++ src/AddonViewer.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
@@ -12,51 +8,34 @@ import type { Addon, AddonVersionConfiguration } from './dataSchemas';
 
 const AddonViewer = ({ addonName, data, selectedAddonVersion, selectedAddonVersionConfiguration }: { addonName: string, data: Addon, selectedAddonVersion: string | null, selectedAddonVersionConfiguration: AddonVersionConfiguration }) => {
 
-  const [tab, setTab] = React.useState('1');
+    return (
+        <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            <Typography variant="h2">
+                { data.addonName }
+            </Typography>
+            <List>
+                <ListItem>
+                    Type: { data.type }
+                </ListItem>
+                <ListItem>
+                    Publisher: { data.publisher }
+                </ListItem>
+                <ListItem>
+                    Owner: { data.owner }
+                </ListItem>
+            </List>
+            <Divider />
 
-  const handleChangeTab = (event: unknown, newValue: string) => {
-      setTab(newValue);
-  };
+            { selectedAddonVersion && selectedAddonVersionConfiguration && <div>
+                <p>Version: { selectedAddonVersion }</p>
 
-  return (
-      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-        <Typography variant="h2">
-          { data.addonName }
-        </Typography>
-        <List>
-          <ListItem>
-            Type: { data.type }
-          </ListItem>
-          <ListItem>
-            Publisher: { data.publisher }
-          </ListItem>
-          <ListItem>
-            Owner: { data.owner }
-          </ListItem>
-        </List>
-        <Divider />
-
-      { selectedAddonVersion && selectedAddonVersionConfiguration && <div>
-          <p>Version: { selectedAddonVersion }</p>
-
-        <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={tab}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChangeTab} aria-label="lab API tabs example">
-            <Tab label="JSONSchema" value="1" />
-            <Tab label="Human" value="2" />
-          </TabList>
+                <Box sx={{ width: '100%', typography: 'body1' }}>
+                    <pre>{ JSON.stringify(selectedAddonVersionConfiguration, null, 2) }</pre>
+                </Box>
+            </div>
+            }
         </Box>
-        <TabPanel value="1">
-          <pre>{ JSON.stringify(selectedAddonVersionConfiguration, null, 2) }</pre>
-        </TabPanel>
-          <TabPanel value="2">TODO</TabPanel>
-      </TabContext>
-    </Box>
-          </div>
-          }
-      </Box>
-  );
+    );
 };
 
 export default AddonViewer;
