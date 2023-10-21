c22148db22c2954353f5a5890812e0f649e8eb9a
Andrew Plummer <plummer574@gmail.com>
2023-10-21 18:56:54 +0100

    Improve viewer and homepage (#14)

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -3,11 +3,27 @@ import Box from '@mui/material/Box';
 import { MuiMarkdown } from 'mui-markdown';
 
 const homepageMd = `
+## EKS Addon Configuration
+
+<br />
+
+---
+
+<br />
+
 EKS Addons have configuration that can be passed as JSON to the <code>configurationValues</code> argument. See:
                 
 https://docs.aws.amazon.com/eks/latest/APIReference/API_CreateAddon.html#AmazonEKS-CreateAddon-request-configurationValues
 
-Documented here are the configuration options that can be passed to each.
+<br />
+
+Documented here are the configuration options that can be passed to each. Many have quite involved JSON Schema, this site provides the option to validate your configuration against the schema. Some schemas for some versions of addons are not actually valid JSON Schema. I don't know how this manifests when passing options when creating the addon.
+
+<br />
+
+---
+
+<br />
 
 Source for this site is at https://github.com/plumdog/eks-addon-configuration
 `;
