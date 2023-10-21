1a2fe45c543bbadb300ed6714a900d3880fb91c0
Andrew Plummer <plummer574@gmail.com>
2023-10-21 14:16:04 +0100

    JSONSchema input validation (#13)

--- src/AddonViewer.tsx
+++ src/AddonViewer.tsx
@@ -4,7 +4,169 @@ import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import JsonView from '@uiw/react-json-view';
 import type { Addon, AddonVersionConfiguration } from './dataSchemas';
+import Ajv2019 from 'ajv/dist/2019';
+import * as draft7MetaSchema from 'ajv/dist/refs/json-schema-draft-07.json';
+import * as draft6MetaSchema from 'ajv/dist/refs/json-schema-draft-06.json';
+
+interface ValidateResult {
+    type: 'bad_json' | 'bad_schema' | 'invalid' | 'valid';
+    message: string;
+}
+
+const ValidateResultViewer = ({ result }: { result: ValidateResult | null }) => {
+    if (result === null) {
+        return null;
+    }
+    if (result.type === 'bad_json') {
+        return <Alert severity="error">
+            Invalid JSON<br />
+            { result.message }
+        </Alert>;
+    }
+    if (result.type === 'bad_schema') {
+        return <Alert severity="error">
+            Invalid schema<br />
+            { result.message }
+        </Alert>;
+    }
+    if (result.type === 'invalid') {
+        return <Alert severity="error">
+            Invalid, see errors below<br />
+            <JsonView value={JSON.parse(result.message)} shortenTextAfterLength={0} />
+        </Alert>;
+    }
+    if (result.type === 'valid') {
+        return <Alert severity="success">
+            Valid
+        </Alert>;
+    }
+    return null;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getValidator = (schema: AddonVersionConfiguration): { validator: any, strictModeFailureMessage: string | null } => {
+    const ajv = new Ajv2019();
+    ajv.addMetaSchema(draft7MetaSchema);
+    ajv.addMetaSchema(draft6MetaSchema);
+    let validator;
+    try {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        validator = ajv.compile(schema as any);
+    } catch (error) {
+        const ajvNonStrict = new Ajv2019({
+            strict: false,
+        });
+        ajvNonStrict.addMetaSchema(draft7MetaSchema);
+        ajvNonStrict.addMetaSchema(draft6MetaSchema);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        validator = ajvNonStrict.compile(schema as any);
+        return {
+            validator,
+            strictModeFailureMessage: String(error),
+        };
+    }
+
+    return {
+        validator,
+        strictModeFailureMessage: null,
+    };
+};
+
+// A JSX component that takes a JSONSchema POJO as input and contains a text area input and a "Validate" button. When pressed, the input is validated against the schema and the result, success or error, is displayed below the text area.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const JSONSchemaValidator = ({ schema }: { schema: AddonVersionConfiguration }) => {
+    const [input, setInput] = React.useState('');
+    const [result, setResult] = React.useState<ValidateResult | null>(null);
+    const [schemaStrictWarning, setSchemaStrictWarning] = React.useState<string | null>(null);
+
+    const validate = () => {
+        try {
+            const parsedInput = JSON.parse(input);
+            // Create a JSON schema validator from the JSON schema POJO using ajv
+            let validator, strictModeFailureMessage;
+            try {
+                const validatorAndStrictMode = getValidator(schema);
+                validator = validatorAndStrictMode.validator;
+                strictModeFailureMessage = validatorAndStrictMode.strictModeFailureMessage;
+            } catch (error) {
+                setResult({
+                    type: 'bad_schema',
+                    message: String(error),
+                });
+                return;
+            }
+
+            setSchemaStrictWarning(strictModeFailureMessage);
+
+            // Validate the input against the schema
+            const valid = validator(parsedInput);
+            if (valid) {
+                setResult({
+                    type: 'valid',
+                    message: 'Valid',
+                });
+            } else {
+                // Display the error message
+                setResult({
+                    type: 'invalid',
+                    message: JSON.stringify(validator.errors, null, 2),
+                });
+            }
+        } catch (error) {
+            let message: string = 'Unknown error';
+            if ((error instanceof Error) && (Object.hasOwnProperty.call(error, 'message'))) {
+                const errorMessage = error.message;
+                if (typeof errorMessage === 'string') {
+                    message = errorMessage;
+                }
+            }
+            setResult({
+                type: 'bad_json',
+                message,
+            });
+        }
+    };
+
+    const pretty = () => {
+        try {
+            const parsedInput = JSON.parse(input);
+            setInput(JSON.stringify(parsedInput, null, 2));
+        } catch (error) {
+            setResult({
+                type: 'bad_json',
+                message: String(error),
+            });
+        }
+    };
+
+    return (
+        <div>
+            <CodeEditor
+                value={input}
+                language="json"
+                aria-label="JSON input"
+                placeholder="JSON input"
+                onChange={(event) => setInput(event.target.value)}
+                style={{
+                    fontSize: 12,
+                    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                }}
+                padding={15}
+            />
+            <button onClick={validate}>Validate</button>
+            <button onClick={pretty}>Pretty</button>
+            <Stack sx={{ width: '100%' }} spacing={2}>
+                <ValidateResultViewer result={result} />
+                { schemaStrictWarning && <Alert severity="info">Schema not parsable in strict mode: { schemaStrictWarning }</Alert> }
+            </Stack>
+        </div>
+    );
+};
 
 const AddonViewer = ({ data, selectedAddonVersion, selectedAddonVersionConfiguration }: { data: Addon, selectedAddonVersion: string | null, selectedAddonVersionConfiguration: AddonVersionConfiguration }) => {
 
@@ -29,8 +191,10 @@ const AddonViewer = ({ data, selectedAddonVersion, selectedAddonVersionConfigura
             { selectedAddonVersion && selectedAddonVersionConfiguration && <div>
                 <p>Version: { selectedAddonVersion }</p>
 
+                <JSONSchemaValidator schema={selectedAddonVersionConfiguration} />
+
                 <Box sx={{ width: '100%', typography: 'body1' }}>
-                    <pre>{ JSON.stringify(selectedAddonVersionConfiguration, null, 2) }</pre>
+                    <JsonView value={selectedAddonVersionConfiguration as object} shortenTextAfterLength={0} />
                 </Box>
             </div>
             }
--- src/dataSchemas.ts
+++ src/dataSchemas.ts
@@ -22,7 +22,7 @@ export type Addon = z.infer<typeof addonSchema>;
 
 const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
 type Literal = z.infer<typeof literalSchema>;
-type Json = Literal | { [key: string]: Json } | Json[];
+export type Json = Literal | { [key: string]: Json } | Json[];
 
 export const jsonSchema: z.ZodType<Json> = z.lazy(() =>
     z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
