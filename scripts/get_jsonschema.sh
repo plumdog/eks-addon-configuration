fe1039b1494030be835545e00ab2a2de92a9ac2c
Andrew Plummer <plummer574@gmail.com>
2023-10-29 09:56:09 +0000

    Revert "Dummy script for debugging update action"
    This reverts commit 6d5bc3fd585f64b7e2da4f26e906ca4d2a191bf8.

--- scripts/get_jsonschema.sh
+++ scripts/get_jsonschema.sh
@@ -6,49 +6,47 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 project_dir="$(dirname "$script_dir")"
 data_dir="$project_dir/data/"
 
-date -Isecond > "$data_dir/date.txt"
+rm -rf "$data_dir"
+mkdir -p "$data_dir"
 
-# rm -rf "$data_dir"
-# mkdir -p "$data_dir"
+addons="$(aws eks describe-addon-versions --region us-east-1 --query addons --output json)"
 
-# addons="$(aws eks describe-addon-versions --region us-east-1 --query addons --output json)"
+aws eks describe-addon-versions --region us-east-1 --query addons --output json | jq 'map(.addonName)' > "$data_dir/addons.json"
 
-# aws eks describe-addon-versions --region us-east-1 --query addons --output json | jq 'map(.addonName)' > "$data_dir/addons.json"
+function writeAddonVersionConfiguration() {
+    addonName="$1"
+    addonVersion="$2"
+    configurationsDir="$3"
+    aws eks describe-addon-configuration --region us-east-1 --addon-name "$addonName" --addon-version "$addonVersion" --query configurationSchema --output text | jq . > "$configurationsDir/$addonVersion.json"
+}
 
-# function writeAddonVersionConfiguration() {
-#     addonName="$1"
-#     addonVersion="$2"
-#     configurationsDir="$3"
-#     aws eks describe-addon-configuration --region us-east-1 --addon-name "$addonName" --addon-version "$addonVersion" --query configurationSchema --output text | jq . > "$configurationsDir/$addonVersion.json"
-# }
+for addonBase64 in $(echo "$addons" | jq -r '.[] | @base64'); do
+    addon="$(echo "$addonBase64" | base64 --decode | jq .)"
+    addonName="$(echo "$addon" | jq -r '.addonName')"
+    addonDir="$data_dir/$addonName/"
+    mkdir -p "$addonDir"
+    echo "$addon" > "$addonDir/addon.json"
 
-# for addonBase64 in $(echo "$addons" | jq -r '.[] | @base64'); do
-#     addon="$(echo "$addonBase64" | base64 --decode | jq .)"
-#     addonName="$(echo "$addon" | jq -r '.addonName')"
-#     addonDir="$data_dir/$addonName/"
-#     mkdir -p "$addonDir"
-#     echo "$addon" > "$addonDir/addon.json"
+    configurationsDir="$addonDir/configurations"
+    mkdir -p "$configurationsDir"
 
-#     configurationsDir="$addonDir/configurations"
-#     mkdir -p "$configurationsDir"
+    for addonVersionBase64 in $(echo "$addon" | jq -r '.addonVersions[] | .addonVersion | @base64'); do
+        addonVersion="$(echo "$addonVersionBase64" | base64 -d)"
+        writeAddonVersionConfiguration "$addonName" "$addonVersion" "$configurationsDir" &
+    done
+done
 
-#     for addonVersionBase64 in $(echo "$addon" | jq -r '.addonVersions[] | .addonVersion | @base64'); do
-#         addonVersion="$(echo "$addonVersionBase64" | base64 -d)"
-#         writeAddonVersionConfiguration "$addonName" "$addonVersion" "$configurationsDir" &
-#     done
-# done
+while true; do
+    jobs_remaining="$(jobs -r | wc -l)"
+    if [[ $jobs_remaining == 0 ]]; then
+        break
+    fi
 
-# while true; do
-#     jobs_remaining="$(jobs -r | wc -l)"
-#     if [[ $jobs_remaining == 0 ]]; then
-#         break
-#     fi
+    echo "$jobs_remaining jobs remaining"
+    sleep 1
+    wait -n
+done
 
-#     echo "$jobs_remaining jobs remaining"
-#     sleep 1
-#     wait -n
-# done
+wait
 
-# wait
-
-# (cd "$data_dir" && find . -name '*.json' | while read filepath; do cat "$filepath" | jq --arg filepath "$filepath" '{$filepath: .}'; done | jq -s 'add' > data.json)
+(cd "$data_dir" && find . -name '*.json' | while read filepath; do cat "$filepath" | jq --arg filepath "$filepath" '{$filepath: .}'; done | jq -s 'add' > data.json)
